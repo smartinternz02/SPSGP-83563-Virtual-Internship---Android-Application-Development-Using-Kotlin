a8f6986f5de919b0f908d6fb5113832919e8d264
anshulagr5 <97024990+anshulagr5@users.noreply.github.com>
2022-09-25 01:54:10 +0530

    Initial Commit

---
+++ Navigation/CupCake/app/src/main/java/com/anshul/cupcake/OrderViewModel.kt
@@ -0,0 +1,93 @@
+package com.anshul.cupcake
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+private const val PRICE_PER_CUPCAKE = 2.00
+
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
+
+class OrderViewModel : ViewModel() {
+
+    // Quantity of cupcakes in this order
+    private val _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> = _quantity
+
+    // Cupcake flavor for this order
+    private val _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> = _flavor
+
+    // Possible date options
+    val dateOptions: List<String> = getPickupOptions()
+
+    // Pickup date
+    private val _date = MutableLiveData<String>()
+    val date: LiveData<String> = _date
+
+    // Price of the order so far
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<String> = Transformations.map(_price) {
+        // Format the price into the local currency and return this as LiveData<String>
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    init {
+        // Set initial values for the order
+        resetOrder()
+    }
+
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+        updatePrice()
+    }
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        // If the user selected the first option (today) for pickup, add the surcharge
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+}
