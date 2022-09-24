3199cb7b0242cc227cf503bf51bcf18ca5289677
anshulagr5 <97024990+anshulagr5@users.noreply.github.com>
2022-09-25 01:53:58 +0530

    Initial Commit

---
+++ Layouts/Words/app/src/main/java/com/anshul/words/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.anshul.words
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.anshul.words.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
+    }
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    val navHostFragment = supportFragmentManager
+        .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    navController = navHostFragment.navController
+}
---
+++ Layouts/Words/app/src/main/java/com/anshul/words/NavigationTests.kt
@@ -0,0 +1,10 @@
+package com.anshul.words
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
+
+class NavigationTests {
+
+
+}
